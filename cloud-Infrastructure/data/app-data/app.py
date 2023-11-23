b85811c2785d7ed962c96627317b4a19284fe6d0
MartimBaptista <martimdsbaptista@gmail.com>
2023-11-23 17:51:47 +0000

    checkpoint just in case

--- cloud-Infrastructure/data/app-data/app.py
+++ cloud-Infrastructure/data/app-data/app.py
@@ -1,7 +1,12 @@
 # Local imports
 import datetime
 
-# Third part imports
+import pandas as pd
+import numpy as np
+
+from sklearn.neighbors import KNeighborsClassifier
+
+# Third party imports
 from flask import Flask
 from flask import request
 import pandas as pd
@@ -14,31 +19,57 @@ from modules.functions import get_model_response
 
 app = Flask(__name__)
 
+def scale01(x):
+  return (x - x.min())/(x.max() - x.min())
 
 @app.route('/health', methods=['GET'])
 def health():
-    """Return service health"""
-    return 'ok'
+  """Return service health"""
+  return 'ok'
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    feature_dict = request.get_json()
-    if not feature_dict:
-        return {
-            'error': 'Body is empty.'
-        }, 500
+  feature_dict = request.get_json()
+  if not feature_dict:
+    return {
+      'error': 'Body is empty.'
+    }, 500
 
-    try:
-        data = []
-        model_name = feature_dict[0]['model']
-        model = joblib.load('model/' + model_name + '.dat.gz')
-        data.append(feature_dict[1])
-        response = get_model_response(data, model)
-    except ValueError as e:
-        return {'error': str(e).split('\n')[-1].strip()}, 500
+  try:
+    data = []
+    model_name = feature_dict[0]['model']
+    model = joblib.load('model/' + model_name + '.dat.gz')
+    data.append(feature_dict[1])
+    response = get_model_response(data, model)
+  except ValueError as e:
+    return {'error': str(e).split('\n')[-1].strip()}, 500
 
-    return response, 200
+  return response, 200
+
+
+@app.route('/train', methods=['GET'])
+def train():
+  # #Loading data
+  # data = np.array(pd.read_csv('data/trainning.data', delimiter=';').values[: , 2:], dtype = float)
+  # (N, d) = data.shape
+
+  # #Scaling data
+  # for var in range(1, 7):
+  #   data[:, var] = scale01(data[:, var])
+
+  # #Spliting Data into inputs (gyro scope data)
+  # inputs = data[:, 1:]
+  # outputs = data[:, 0]
+
+  #Fitting the model
+  #KNN = KNeighborsClassifier(n_neighbors = 3, weights = 'uniform')
+  #KNN.fit(inputs, outputs)
+
+  #Saving the model
+  #joblib.dump(KNN, 'model/KKN')
+
+  return "training..."
 
 
 if __name__ == '__main__':
