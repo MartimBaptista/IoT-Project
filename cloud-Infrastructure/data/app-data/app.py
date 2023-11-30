b7185de6c81f353992b20352f10bb97de6413469
MartimBaptista <martimdsbaptista@gmail.com>
2023-11-30 15:58:31 +0000

    Scaling and training fully done

--- cloud-Infrastructure/data/app-data/app.py
+++ cloud-Infrastructure/data/app-data/app.py
@@ -3,6 +3,7 @@ import datetime
 
 import pandas as pd
 import numpy as np
+import os
 
 from sklearn.neighbors import KNeighborsClassifier
 
@@ -14,60 +15,46 @@ import joblib
 import gzip
 import json
 
-from modules.functions import get_model_response
-
-
 app = Flask(__name__)
 
-def scaleData(x):
-  return (x - x.min())/(x.max() - x.min())
+def scaleData(x, min, max):
+  return (x - min)/(max - min)
 
-@app.route('/health', methods=['GET'])
-def health():
-  """Return service health"""
-  return 'ok'
+#TODO: REMOVE ME, eventually
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#   feature_dict = request.get_json()
+#   if not feature_dict:
+#     return {
+#       'error': 'Body is empty.'
+#     }, 500
 
+#   try:
+#     data = []
+#     model_name = feature_dict[0]['model']
+#     model = joblib.load('model/' + model_name + '.dat.gz')
+#     data.append(feature_dict[1])
+#     print(feature_dict)
+#     response = get_model_response(data, model)
+#   except ValueError as e:
+#     return {'error': str(e).split('\n')[-1].strip()}, 500
 
-@app.route('/predict', methods=['POST'])
-def predict():
-  feature_dict = request.get_json()
-  if not feature_dict:
-    return {
-      'error': 'Body is empty.'
-    }, 500
+#   return response, 200
 
-  try:
-    data = []
-    model_name = feature_dict[0]['model']
-    model = joblib.load('model/' + model_name + '.dat.gz')
-    data.append(feature_dict[1])
-    print(feature_dict)
-    response = get_model_response(data, model)
-  except ValueError as e:
-    return {'error': str(e).split('\n')[-1].strip()}, 500
-
-  return response, 200
-
-@app.route('/predict_new', methods=['POST'])
-def predict_new():
-  #check that its not empty and valid
-  data = list(map(float, request.data.decode("utf-8").split(",")))
-  #TODO: findout how to scale this?
-  #Very that the file exists
-  model = joblib.load('model/KKN.dat.gz')
-
-  return str(model.predict([data])), 200
-
-
-@app.route('/train', methods=['GET'])
+@app.route('/train', methods=['POST'])
 def train():
+
+  #Loading data from request into file
+  file = open("data/trainning.data", "w")
+  file.write(request.data.decode("utf-8"))
+  file.close()
+
   #Loading data
   data = np.array(pd.read_csv('data/trainning.data', delimiter=';').values[: , 2:], dtype = float)
-  (N, d) = data.shape
 
-  #Scaling data TODO: find out how to scale the received data
-  #for var in range(1, 7):
-  #  data[:, var] = scaleData(data[:, var])
+  #Calculating the mins and maxs for later scaling and scaling training data
+  for var in range(1, 7):
+    data[:, var] = scaleData(data[:, var], data[:, var].min(), data[:, var].max())
 
   #Spliting Data into inputs (gyro scope data)
   inputs = data[:, 1:]
@@ -82,6 +69,21 @@ def train():
 
   return "ok", 200
 
+@app.route('/predict', methods=['POST'])
+def predict():
+  #Check for model and data file
+  if os.path.exists(os.path.abspath("data/trainning.data")) and os.path.exists(os.path.abspath("model/KKN.dat.gz")):
+    train_data = np.array(pd.read_csv('data/trainning.data', delimiter=';').values[: , 3:], dtype = float)
+    data = list(map(float, request.data.decode("utf-8").split(",")))
+
+    for var in range(6):
+      data[var] = scaleData(data[var], train_data[:, var].min(), train_data[:, var].max())
+
+    #Verify that the file exists
+    model = joblib.load('model/KKN.dat.gz')
+    return str(model.predict([data])), 200
+  else:
+    return "No model has trained", 409
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+  app.run(host='0.0.0.0')
