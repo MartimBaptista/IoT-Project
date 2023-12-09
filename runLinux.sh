0dc6f4d7c74d07d19f6c0fc47e7504c21ae07a6f
MartimBaptista <martimdsbaptista@gmail.com>
2023-12-09 16:15:39 +0000

    small

--- runLinux.sh
+++ runLinux.sh
@@ -1,4 +1,1 @@
-#print to console advising we will be freeing a port for our app
-echo "We will be shutting down a program running on port 5432, beware"
-sudo kill -9 $(sudo lsof -ti:5432)
 docker-compose -f cloud-Infrastructure/docker-compose.yml up
